5844925613d468b8a917a53868520c2b37b652c0
Brenno Cavalcante <brennofaby@gmail.com>
2023-04-24 23:17:34 -0300

    feat: added seed data to Category table

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from urllib.parse import unquote
 from sqlalchemy.exc import IntegrityError
 from flask import redirect
 from flask_cors import CORS
 from flask_openapi3 import OpenAPI, Info, Tag
+from sqlalchemyseeder import ResolvingSeeder
 from model import Session, Camera, Category
 
 from schemas import *
@@ -21,13 +21,21 @@ camera_tag = Tag(
 category_tag = Tag(
     name="Categoria", description="Adição de categoria de câmeras.")
 
+# coloca dados na tabela Categoria caso esteja vazia
+session = Session()
+if not session.query(Category).first():
+    seeder = ResolvingSeeder(session)
+    register_categories = seeder.register('model:Category')
+    new_entities = seeder.load_entities_from_json_file("seeds/categories.json")
+    session.commit()
+
 
 @app.get("/", tags=[home_tag])
 def home():
     """
     Redireciona para /openapi para escolha de estilo de documentação.
     """
-    return redirect("/openapi")
+    return redirect("/openapi/swagger")
 
 
 @app.post("/camera", tags=[camera_tag], responses={"200": CameraViewSchema, "409": ErrorSchema, "400": ErrorSchema, })
@@ -58,7 +66,6 @@ def add_camera(form: CameraSchema):
     camera = Camera(
         name=form.name,
         brand=form.brand,
-        description=form.description,
         category_id=form.category_id,
         value=form.value,
     )
@@ -93,7 +100,8 @@ def get_cameras():
     """
     logger.debug(f"Coletando câmeras")
     session = Session()
-    cameras = session.query(Camera).all()
+    cameras = session.query(Camera).join(
+        Category).order_by(Camera.created_at.asc()).all()
 
     if not cameras:
         return {"cameras": []}, 200
@@ -127,24 +135,24 @@ def get_camera(query: CameraSearchSchema):
 @app.delete("/camera", tags=[camera_tag], responses={"200": CameraDelSchema, "404": ErrorSchema})
 def del_camera(query: CameraSearchSchema):
     """
-    Deleta uma Câmera a partir do nome.
+    Deleta uma Câmera a partir do id.
 
     Retorna uma mensagem de confirmação de remoção.
     """
-    camera_name = unquote(unquote(query.name))
-    print(camera_name)  # Precisa disso aqui?
-    logger.debug(f"Deletando dados da câmera #{camera_name}")
+    camera_id = query.id
+    logger.debug(f"Deletando dados da câmera de id #{camera_id}")
 
     session = Session()
-    count = session.query(Camera).filter(Camera.name == camera_name).delete()
+    count = session.query(Camera).filter(Camera.id == camera_id).delete()
     session.commit()
 
     if count:
-        logger.debug(f"Apagando câmera #{camera_name}")
-        return {"message": "Câmera apagada!", "id": camera_name}
+        logger.debug(f"Apagando câmera #{camera_id}")
+        return {"message": "Câmera apagada!", "id": camera_id}
     else:
         error_msg = "Câmera não encontrada..."
-        logger.warning(f"Erro ao deletar câmera '{camera_name}', {error_msg}")
+        logger.warning(
+            f"Erro ao deletar câmera de id '{camera_id}', {error_msg}")
         return {"message": error_msg}, 404
 
 
@@ -179,3 +187,21 @@ def add_category(form: CategorySchema):
         logger.warning(
             f"Erro ao adicionar a categoria '{category.name}', {error_msg}")
         return {"message": error_msg}, 400
+
+
+@app.get("/categories", tags=[category_tag], responses={"200": CategoryViewSchema, "404": ErrorSchema})
+def get_category():
+    """
+    Retorna todas as categorias de câmeras.
+
+    Retorna uma representação da listagem de categorias
+    """
+    logger.debug(f"Coletando câmeras")
+    session = Session()
+    categories = session.query(Category).all()
+
+    if not categories:
+        return {"categories": []}, 200
+    else:
+        logger.debug(f"%d câmeras encontradas" % len(categories))
+        return show_categories(categories), 200
